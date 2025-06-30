a1db9e0be560ee8b3ebe59241fd25ac5f57b2992
Hideaki-Umezawa <hideaki_umezawa@mail.toyota.co.jp>
2025-06-30 23:01:14 +0900

    front環境変数変更(axios.js)

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 //環境によって切り替えるurlとCoolieとかリクエスト
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 })
 
