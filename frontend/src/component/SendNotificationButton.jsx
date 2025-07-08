a7afc99c81fcc980dfb67f98eda011dabdb76bd5
Hideaki-Umezawa <hideaki_umezawa@mail.toyota.co.jp>
2025-07-08 16:04:16 +0900

    VITE_ONESIGNAL_API_KEY追加

--- frontend/src/component/SendNotificationButton.jsx
+++ frontend/src/component/SendNotificationButton.jsx
@@ -45,13 +45,7 @@ export default SendNotificationButton;
 //
 // const SendNotificationButton = () => {
 //     const sendNotification = async () => {
-//         const restApiKey = 'os_v2_app_auuc3i3i5vd3tm6ccjtvsxelbfndbus3bgzunnmscru63y6teypfumubtdhck7zzfo3z4dqs2xp5uqd7w3k4g3kdecjkzn5nwoicwwy'; // ←安全な場所に保管するべき
 //         const appId = '05282da3-68ed-47b9-b3c2-1267595c8b09';
-//         const targetPlayerId = [
-//             '0fa068ed-edf9-494f-81b6-dbd27d11f8d5',
-//             '69454c49-9fdb-48b3-8dd6-aa3abbd3a4a3',
-//             '771a5842-c933-4bdd-b6e8-90a0f8569992',
-//         ];  // ← Subscription ID に変更
 //         try {
 //             const response = await axios.post(
 //                 'https://onesignal.com/api/v1/notifications',
