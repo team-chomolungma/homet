62327a623b2a4198184d537efef3addec205d838
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-07-08 13:56:56 +0900

    Fix:responsive config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,27 +13,46 @@ import Home from './component/Home.jsx';
 import {AuthProvider} from './component/safety/AuthContext.jsx'
 import SessionChecker from './component/safety/SessionChecker.jsx';
 import OneSignalPush from './component/OneSignalPush.jsx';
+import AddFriend from './component/friend/AddFriend.jsx';
+import Friendlist from './component/friend/Friendlist.jsx';
+import AudioSendAfter from './component/Audio/AudioSendAfter.jsx';
+import {createTheme} from '@mui/material';
+import {ThemeProvider} from '@mui/material/styles';
 
+const theme = createTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 390,
+            md: 430,
+        },
+    },
+});
 
 window.global = window;
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter>
-            <AuthProvider>
-                <SessionChecker>
-                    <OneSignalPush/>
-                    <Routes>
-                        <Route path="/" element={<StartForm/>}/> {/* 最初 */}
-                        <Route path="/start" element={<Start/>}/> {/* start */}
-                        <Route path="/login" element={<Login/>}/> {/* login */}
-                        <Route path="/signup" element={<Signup/>}/> {/* 新規作成 */}
-                        <Route path="/home" element={<Home/>}/> {/* 新規作成 */}
-                        <Route path="/voice" element={<AudioRecording/>}/> {/* 新規作成 */}
-                        <Route path="/voice-data" element={<AudioListen/>}/>
-                        <Route path="/timeline" element={<Timeline/>}/>
-                    </Routes>
-                </SessionChecker>
-            </AuthProvider>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <AuthProvider>
+                    <SessionChecker>
+                        <OneSignalPush/>
+                        <Routes>
+                            <Route path="/" element={<StartForm/>}/> {/* 最初 */}
+                            <Route path="/start" element={<Start/>}/> {/* start */}
+                            <Route path="/login" element={<Login/>}/> {/* login */}
+                            <Route path="/signup" element={<Signup/>}/> {/* 新規作成 */}
+                            <Route path="/home" element={<Home/>}/> {/* 新規作成 */}
+                            <Route path="/voice" element={<AudioRecording/>}/> {/* 新規作成 */}
+                            <Route path="/voice-data" element={<AudioListen/>}/>
+                            <Route path="/voice-after" element={<AudioSendAfter/>}/>
+                            <Route path="/timeline" element={<Timeline/>}/>
+                            <Route path="/friendlist" element={<Friendlist/>}/>
+                            <Route path="/addfriend" element={<AddFriend/>}/>
+                        </Routes>
+                    </SessionChecker>
+                </AuthProvider>
+            </BrowserRouter>
+        </ThemeProvider>
     </StrictMode>
 )
