52ec18fd853c7a48f555e75f0c696674ad2dce54
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-07-10 17:04:47 +0900

    fix bug

--- backend/src/main/kotlin/com/example/homet/filter/AuthFilter.kt
+++ backend/src/main/kotlin/com/example/homet/filter/AuthFilter.kt
@@ -15,7 +15,7 @@ data class AuthFilter(
     override fun shouldNotFilter(request: HttpServletRequest): Boolean {
         val path = request.servletPath
         return request.method.equals("OPTIONS", ignoreCase = true) ||
-                path.startsWith("/api/auth/login") || path.startsWith("/api/auth/signup") || path.startsWith("/")
+                path.startsWith("/api/auth/login") || path.startsWith("/api/auth/signup")  || path == "/index.html"
     }
     override fun doFilterInternal(
         request: HttpServletRequest,
